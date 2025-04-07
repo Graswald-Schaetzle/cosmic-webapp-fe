801d3f841a964e473ec2ba14fa5ed3193f479913
dmytro-velychko-2ops <dmytro.velychko@2ops.io>
2025-04-07 15:49:26 +0300

    merge dev

---
+++ src/html-markup.js
@@ -0,0 +1,12 @@
+const commonElements = {
+    matterportIframe: document.querySelector('#showcase-iframe'),
+    createTaskButton: document.querySelector('#create-task-button'),
+    tagControlContainer: document.querySelector('.tag-control-container'),
+    tagControlContainerBg: document.querySelector('.tag-control-container .bg'),
+}
+
+
+
+export const htmlMarkup = {
+    commonElements
+}
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import {initMatterport} from "./matterport";
+import {initTasks} from "./tasks";
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    initMatterport();
+    initTasks();
+});
+
+
+
